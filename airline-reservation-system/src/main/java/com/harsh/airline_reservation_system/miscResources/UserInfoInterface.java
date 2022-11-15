fb04568c68afa66aff130c6e186e4d44daee628a
Harsh Morayya <harshmorayya@ymail.com>
2022-11-15 22:05:48 +0530

    Finishing touches

---
+++ airline-reservation-system/src/main/java/com/harsh/airline_reservation_system/miscResources/UserInfoInterface.java
@@ -0,0 +1,8 @@
+package com.harsh.airline_reservation_system.miscResources;
+
+import com.harsh.airline_reservation_system.UserInfo;
+
+public interface UserInfoInterface {
+	public void setUserProperties(UserInfo userInfo);
+	public UserInfo getUserProperties();
+}
