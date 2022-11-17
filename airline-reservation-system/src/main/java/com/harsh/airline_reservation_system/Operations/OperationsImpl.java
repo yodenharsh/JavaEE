fa5e0861a35e169298ac12663fd7bb4db669b100
Harsh Morayya <harshmorayya@ymail.com>
2022-11-17 15:52:37 +0530

    Now uses jasypt for encryption

--- airline-reservation-system/src/main/java/com/harsh/airline_reservation_system/Operations/OperationsImpl.java
+++ airline-reservation-system/src/main/java/com/harsh/airline_reservation_system/Operations/OperationsImpl.java
@@ -5,17 +5,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.jasypt.util.password.BasicPasswordEncryptor;
+
 import com.harsh.airline_reservation_system.UserInfo;
 
 public class OperationsImpl implements AuthOperationsInterface {
 
 	@Override
 	public boolean login(UserInfo userInfo, Connection conn) {
-		String query = "SELECT email, password FROM userList where email = '" + userInfo.email()
-				+ "'and password = MD5('" + userInfo.password() + "')";
+		String query = "SELECT email, password FROM userList where email = '" + userInfo.email() + "'";
 		try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
-			if (rs.next())
-				return true; // email password combination is present
+			if (rs.next()) {
+				String encPswd = rs.getString("password");
+				boolean samePswd = checkPassword(encPswd, userInfo.password());
+				if (samePswd)
+					return true;
+			}
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -34,8 +39,9 @@ public class OperationsImpl implements AuthOperationsInterface {
 
 			if (rs.next()) // rows were returned - email exists in database
 				return false;
+			String encryptedPassword = getEncrypted(userInfo.password());
 
-			query = "INSERT INTO userList VALUES ('" + userInfo.email() + "', MD5('" + userInfo.password() + "'))";
+			query = "INSERT INTO userList VALUES ('" + userInfo.email() + "', '" + encryptedPassword + "')";
 			if (stmt.executeUpdate(query) == 1)
 				return true;
 
@@ -48,11 +54,14 @@ public class OperationsImpl implements AuthOperationsInterface {
 
 	@Override
 	public boolean changePassword(UserInfo userInfo, String newPassword, Connection conn) {
-		String query = "UPDATE userList SET password = MD5('"+newPassword+"') WHERE email = '"+userInfo.email()+"'";
-		try(Statement stmt = conn.createStatement()){
-			 int affectedRows = stmt.executeUpdate(query);
-			 if(affectedRows == 1) return true;
-		} catch(SQLException e) {
+		String encryptedPassword = getEncrypted(newPassword);
+		String query = "UPDATE userList SET password = '" + encryptedPassword + "' WHERE email = '" + userInfo.email()
+				+ "'";
+		try (Statement stmt = conn.createStatement()) {
+			int affectedRows = stmt.executeUpdate(query);
+			if (affectedRows == 1)
+				return true;
+		} catch (SQLException e) {
 			e.printStackTrace();
 			return false;
 		}
@@ -61,7 +70,20 @@ public class OperationsImpl implements AuthOperationsInterface {
 
 	@Override
 	public boolean forgotPassword(String email, Connection conn) {
-		System.out.println("Please check your inbox for a changing password, " + email);		return false;
+		System.out.println("Please check your inbox for a changing password, " + email);
+		return false;
+	}
+
+	private static String getEncrypted(String str) {
+		BasicPasswordEncryptor passwordEncryptor = new BasicPasswordEncryptor();
+		String encryptedPassword = passwordEncryptor.encryptPassword(str);
+		System.out.println(encryptedPassword);
+		return encryptedPassword;
+	}
+
+	private static boolean checkPassword(String encPassword, String enteredPassword) {
+		BasicPasswordEncryptor passwordEncryptor = new BasicPasswordEncryptor();
+		return passwordEncryptor.checkPassword(enteredPassword, encPassword);
 	}
 
 }
